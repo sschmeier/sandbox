ab532ce4c306cc9a80b8db5e823393617516d6a5
Sebastian Schmeier <s.schmeier@gmail.com>
2017-11-07 09:28:26 +1300

    Added envens and signal table

--- hdf5test/poreqc.py
+++ hdf5test/poreqc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
-NAME: {{name}}
-=========
+NAME: poreqc.py
+===============
 
 DESCRIPTION
 ===========
@@ -34,43 +34,51 @@ import gzip
 import bz2
 import zipfile
 import time
-
-try:
-    from colorama import init, Fore, Style
-except ImportError:
-    sys.stderr.write('colorama lib needed.\n')
-    sys.exit(1)
-
-# INIT color
-# Initialise colours for multi-platform support.
-init()
+import h5py
+import numpy as np
 
 # When piping stdout into head python raises an exception
 # Ignore SIG_PIPE and don't throw exceptions on it...
 # (http://docs.python.org/library/signal.html)
 signal(SIGPIPE, SIG_DFL)
 
-__version__ = '{{version}}'
-__date__ = '{{date}}'
-__email__ = '{{email}}'
-__author__ = '{{author}}'
+__version__ = 'v0.1'
+__date__ = '2017/10/30'
+__email__ = 's.schmeier@gmail.com'
+__author__ = 'Sebastian Schmeier'
 
+
+# For color handling on the shell
+try:
+    from colorama import init, Fore, Style
+    # INIT color
+    # Initialise colours for multi-platform support.
+    init()
+    reset=Fore.RESET
+    colors = {'success': Fore.GREEN, 'error': Fore.RED, 'warning': Fore.YELLOW, 'info':''}
+except ImportError:
+    sys.stderr.write('colorama lib desirable. Install with "conda install colorama".\n\n')
+    reset=''
+    colors = {'success': '', 'error': '', 'warning': '', 'info':''}
 
 def alert(atype, text, log):
-    d = {'success': Fore.GREEN, 'error': Fore.RED, 'warning': Fore.YELLOW, 'info':''}
     textout = '%s [%s] %s\n' % (time.strftime('%Y%m%d-%H:%M:%S'),
                                 atype.rjust(7),
                                 text)
-    log.write('%s%s%s' % (d[atype], textout, Fore.RESET))
+    log.write('%s%s%s' % (colors[atype], textout, reset))
     if atype=='error': sys.exit()
+        
 def success(text, log=sys.stderr):
     alert('success', text, log)
+    
 def error(text, log=sys.stderr):
     alert('error', text, log)
+    
 def warning(text, log=sys.stderr):
     alert('warning', text, log)
+    
 def info(text, log=sys.stderr):
-    alert('info', text, log)    
+    alert('info', text, log)  
 
     
 def parse_cmdline():
@@ -113,27 +121,12 @@ def parse_cmdline():
     return args, parser
 
 
-def load_file(filename):
-    """ LOADING FILES """
-    if filename in ['-', 'stdin']:
-        filehandle = sys.stdin
-    elif filename.split('.')[-1] == 'gz':
-        filehandle = gzip.open(filename)
-    elif filename.split('.')[-1] == 'bz2':
-        filehandle = bz2.BZFile(filename)
-    elif filename.split('.')[-1] == 'zip':
-        filehandle = zipfile.Zipfile(filename)
-    else:
-        filehandle = open(filename)
-    return filehandle
-
-
 def main():
     """ The main funtion. """
     args, parser = parse_cmdline()
 
     try:
-        fileobj = load_file(args.str_file)
+        f5 = h5py.File(args.str_file, "r")
     except:
         error('Could not load file. EXIT.')
 
@@ -147,15 +140,45 @@ def main():
     else:
         outfileobj = open(args.outfile_name, 'w')
 
+    # some info
+    #TRACKING_ID = "/UniqueGlobalKey/tracking_id"
+    #for k,v in f5[TRACKING_ID].attrs.items():
+    #    print('%s\t%s'%(k,str(v)))
 
-    # delimited file handler
-    csv_reader_obj = csv.reader(fileobj, delimiter=args.delimiter_str)
+    #CONTEXT_TAGS = "/UniqueGlobalKey/context_tags"
+    #for k,v in f5[CONTEXT_TAGS].attrs.items():
+    #    print('%s\t%s'%(k,str(v)))
+
+    ## Raw read signal
+    #RAW_READS = '/Raw/Reads/'
+    #read_subgroup = []
+    #f5[RAW_READS].visit(read_subgroup.append)
+    #print(read_subgroup)
+    #for k,v in f5['%s/%s'%(RAW_READS, read_subgroup[0])].attrs.items():
+    #    print('%s\t%s'%(k,str(v)))
+
     
-    # WORK FROM HERE:
+    # FASTQ
+    #FASTQ = 'Analyses/Basecall_1D_000/BaseCalled_template/Fastq'
+    #print(f5[FASTQ])
+
+    #from pandas import read_hdf
+    #hdf = read_hdf(args.str_file)
+    #print(hdf)
+
+    # print all groups and attr.
+    f5.visititems(print_attrs_for_all_group)
 
     # ------------------------------------------------------
     outfileobj.close()
     return
+
+
+def print_attrs_for_all_group(name, obj):
+    print(name)
+    for key, val in obj.attrs.items():
+        print("\t%s: %s" % (key, val))
+
 
 
 if __name__ == '__main__':
